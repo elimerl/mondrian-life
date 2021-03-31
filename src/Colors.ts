ac81ca287119893473a20ef599c2c35799607ab5
elimerl <elimerlindavies@gmail.com>
2021-03-31 15:26:15 -0700

    first commit

---
+++ src/Colors.ts
@@ -0,0 +1,7 @@
+export enum Colors {
+  WHITE = 'rgb(249 245 234)',
+  RED = 'rgb(224 165 157)',
+  BLUE = 'rgb(160 187 216)',
+  YELLOW = 'rgb(254 242 70)',
+  GREY = 'rgb(178 180 175)',
+}
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import { Colors } from './Colors';
+import { Game } from './GameOfLife';
+const ctx = (document.getElementById('canvas') as HTMLCanvasElement).getContext(
+  '2d',
+) as CanvasRenderingContext2D;
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+const sample = <T>(arr: T[]) => arr[Math.floor(Math.random() * arr.length)];
+const game = new Game(
+  Array.from({ length: Math.floor((window.innerWidth * 3) / 75) }, () => {
+    return Array.from(
+      { length: Math.floor((window.innerHeight * 3) / 75) },
+      () => {
+        return 0;
+      },
+    );
+  }),
+);
+function set(x: number, y: number, v: 1 | 0) {
+  game.board[y][x] = v;
+}
+set(1, 0, 1);
+set(2, 1, 1);
+set(0, 2, 1);
+set(1, 2, 1);
+set(2, 2, 1);
+
+const colors = Array.from(
+  { length: Math.floor((window.innerWidth * 3) / 75) },
+  () => {
+    return Array.from(
+      { length: Math.floor((window.innerHeight * 3) / 75) },
+      () => {
+        return sample([Colors.RED, Colors.BLUE, Colors.YELLOW, Colors.GREY]);
+      },
+    );
+  },
+);
+
+setInterval(frame, 50);
+let paused = false;
+function frame() {
+  if (!paused) game.step();
+  ctx.fillStyle = Colors.WHITE;
+
+  ctx.fillRect(0, 0, window.innerWidth * 3, window.innerHeight * 3);
+  render();
+}
+function render() {
+  game.board.forEach((row, y) => {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(y * 78 - 3, 0, 3, window.innerHeight * 3);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, y * 78 - 3, window.innerWidth * 3, 3);
+
+    row.forEach((v, x) => {
+      if (v === 1) {
+        ctx.fillStyle = colors[y][x];
+
+        ctx.fillRect(x * 78, y * 78, 75, 75);
+      }
+    });
+  });
+}
+
+document.onkeydown = (ev) => {
+  if (ev.key === 'p') {
+    paused = !paused;
+  }
+  if (ev.key === 's') {
+    const a = document.createElement('a');
+    a.download = 'life.png';
+    a.href = ctx.canvas.toDataURL();
+    a.click();
+  }
+};
+ctx.scale(1 / 3, 1 / 3);
