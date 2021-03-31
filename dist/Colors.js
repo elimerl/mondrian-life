bb4ab03ee39381dcf34c732f6cd7286115830a6d
elimerl <elimerl@users.noreply.github.com>
2021-03-31 22:26:37 +0000

    deploy: ac81ca287119893473a20ef599c2c35799607ab5

---
+++ dist/Colors.js
@@ -0,0 +1,8 @@
+export var Colors;
+(function(Colors2) {
+  Colors2["WHITE"] = "rgb(249 245 234)";
+  Colors2["RED"] = "rgb(224 165 157)";
+  Colors2["BLUE"] = "rgb(160 187 216)";
+  Colors2["YELLOW"] = "rgb(254 242 70)";
+  Colors2["GREY"] = "rgb(178 180 175)";
+})(Colors || (Colors = {}));
---
+++ dist/GameOfLife.js
@@ -0,0 +1,24 @@
+export class Game {
+  constructor(board) {
+    this.board = board;
+  }
+  step() {
+    this.board = this.board.map((row, cellY) => {
+      return row.map((cell, cellX) => {
+        let numberOfLiveNeighbours = 0;
+        for (let y = cellY - 1; y <= cellY + 1; y++) {
+          for (let x = cellX - 1; x <= cellX + 1; x++) {
+            if (y === cellY && x === cellX)
+              continue;
+            numberOfLiveNeighbours += this.board?.[y]?.[x] || 0;
+          }
+        }
+        if (cell && [2, 3].includes(numberOfLiveNeighbours))
+          return 1;
+        if (!cell && numberOfLiveNeighbours === 3)
+          return 1;
+        return 0;
+      });
+    });
+  }
+}
